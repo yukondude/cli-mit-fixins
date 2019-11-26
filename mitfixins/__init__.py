7923eb45153554b4fe157c483259e0d86cb6a31b
Dave Rogers <thedude@yukondude.com>
2019-11-25 21:03:47 -0800

    Prepare public interface for package.

---
+++ mitfixins/__init__.py
@@ -0,0 +1,25 @@
+""" Helpers for Click-based Python command line interfaces with features up the wazoo
+    (see cookiecutter-cli-mit-fixins).
+"""
+
+# This file is part of cli-mit-fixins.
+# Copyright Dave Rogers 2019 <thedude@yukondude.com>.
+# Licensed under the GNU General Public License, version 3.
+# Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
+
+from .config import config_command_class, handle_print_config_option
+from .constants import (
+    COMMAND_NAME,
+    DEFAULT_CONFIG_FILE_PATH,
+    DEFAULT_PRINT_CONFIG_OPTION,
+    DEFAULT_CONFIG_FILE_OPTION,
+)
+from .decorators import (
+    cli_config_file_option,
+    cli_dry_run_option,
+    cli_print_config_option,
+    cli_verbose_option,
+    cli_version_option,
+)
+from .exceptions import CliException
+from .utility import echo_wrapper
